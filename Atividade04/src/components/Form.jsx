d6d697d05d0c0f42f41f12e59e4dcd34eb15752a
d4v1dm7rs <ds093461@gmail.com>
2025-09-05 22:38:34 -0300

    Atualizando a atividade

--- Atividade04/src/components/Form.jsx
+++ Atividade04/src/components/Form.jsx
@@ -1,48 +1,118 @@
-import {useState} from "react"
+import { useState } from "react"
 function Form() {
 
-    function cadastrarUsuario(event){
+    function cadastrarUsuario(event) {
         event.preventDefault()
-        console.log(`Usuário ${name} foi cadastrado com a senha ${password} e com o endereço ${endereco}`)
+        console.log(`Usuário ${name}, com o sobrenome ${lastName} foi cadastrado com a senha ${password}, com o  e-mail${email}`)
     }
 
-    const [name, setName]=useState()
-    const [password, setPassword]=useState()
-    const [endereco, setEndereco]=useState()
+    const [name, setName] = useState()
+    const [lastName, setLastName] = useState()
+    const [password, setPassword] = useState()
+    const [email, setEmail] = useState()
 
-    return(
+    return (
         <div className="container">
-            <h1>Meu Cadastro:</h1>
-                <form onSubmit={cadastrarUsuario}>
-                <div>
-                    <label htmlFor="name">Nome:</label>
-                    <input 
-                    type="text" 
-                    id="name" name="name" 
-                    placeholder="Digite o seu nome:" 
-                    onChange={(event) => setName(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="endereco">Endereço:</label>
-                    <input type="text"
-                    id="endereco" 
-                    name="endereco" 
-                    placeholder="Digite o seu endereço" 
-                    onChange={(event) => setEndereco(event.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="password">Senha:</label>
-                    <input type="password"
-                    id="password" 
-                    name="password" 
-                    placeholder="Digite a sua senha:" 
-                    onChange={(event) => setPassword(event.target.value)}/>
-                </div>
-                <div>
-                    <input type="submit" value="Cadastrar"/>
-                </div>
-                </form>
+            <form onSubmit={cadastrarUsuario}>
+                <fieldset>
+                    <legend>Cadastro de Usuário</legend>
+                    <h2>Nome completo</h2>
+                    <div>
+                        <label htmlFor="name">Primeiro nome:</label>
+                        <input
+                            type="text"
+                            id="name" name="name"
+                            placeholder="Digite o seu primeiro nome:"
+                            onChange={(event) => setName(event.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="name">Sobrenome:</label>
+                        <input
+                            type="text"
+                            id="lastName" name="LastName"
+                            placeholder="Digite o seu sobrenome:"
+                            onChange={(event) => setLastName(event.target.value)}
+                        />
+                    </div>
+                    <h2>E-mail</h2>
+                    <div>
+                        <label htmlFor="email">E-mail:</label>
+                        <input type="text"
+                            id="email"
+                            name="email"
+                            placeholder="Digite o seu e-mail"
+                            onChange={(event) => setEmail(event.target.value)} />
+                    </div>
+                    <h2>Número do Telefone</h2>
+                    <div>
+                        <label htmlFor="email">Telefone:</label>
+                        <input type="text"
+                            id="email"
+                            name="email"
+                            placeholder="Digite o seu e-mail"
+                            onChange={(event) => setEmail(event.target.value)} />
+                    </div>
+                    <h2>Endereço</h2>
+                    <div>
+                        <label htmlFor="password">País:</label>
+                        <select name="" id="">
+                            <option value="EUA">EUA</option>
+                            <option value="Islândia">Islândia</option>
+                            <option value="Brasil">Brasil</option>
+                            <option value="Chipre">Chipre</option>
+                            <option value="Chile">Chile</option>
+                            <option value="Argentina">Argentina</option>
+                            <option value="Canadá">Canadá</option>
+                            <option value="">Canadá</option>
+                        </select> 
+                    </div>
+                    <div>
+                    <div>
+                        <label htmlFor="password">Estado:</label>
+                        <input type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Digite a sua senha:"
+                            onChange={(event) => setPassword(event.target.value)} />
+                    </div>
+                        <label htmlFor="password">Cidade:</label>
+                        <input type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Digite a sua senha:"
+                            onChange={(event) => setPassword(event.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">CEP:</label>
+                        <input type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Digite a sua senha:"
+                            onChange={(event) => setPassword(event.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Rua:</label>
+                        <input type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Digite a sua senha:"
+                            onChange={(event) => setPassword(event.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Número:</label>
+                        <input type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Digite a sua senha:"
+                            onChange={(event) => setPassword(event.target.value)} />
+                    </div>
+                    <div>
+                        <input type="submit" value="Cadastrar" />
+                    </div>
+                </fieldset>
+            </form>
+
         </div>
     )
 }
